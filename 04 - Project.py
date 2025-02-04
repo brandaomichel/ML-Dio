663921b3bd45f626ac8e0c90c0a398527c36f7f6
Michel <michelgbo@gmail.com>
2025-02-04 16:43:34 -0300

    pjt04

---
+++ 04 - Project.py
@@ -0,0 +1,30 @@
+from google.colab import drive
+drive.mount('/content/gdrive')
+
+def imShow(path):
+  import cv2
+  import matplotlib.pyplot as plt
+  %matplotlib inline
+
+  image = cv2.imread(path)
+  height, width = image.shape[:2]
+  resized_image = cv2.resize(image,(3*width, 3*height), interpolation = cv2.INTER_CUBIC)
+
+  fig = plt.gcf()
+  fig.set_size_inches(18, 10)
+  plt.axis("off")
+  #plt.rcParams['figure.figsize'] = [10, 5]
+  plt.imshow(cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB))
+  plt.show()
+  
+  
+def upload():
+  from google.colab import files
+  uploaded = files.upload() 
+  for name, data in uploaded.items():
+    with open(name, 'wb') as f:
+      f.write(data)
+      print ('saved file', name)
+def download(path):
+  from google.colab import files
+  files.download(path)
